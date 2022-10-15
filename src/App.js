c00458e26e36fbe15b35e3cbb676aef7fa5e8d6c
Ahmed Faheem <ahmedfaheemmalick@gmail.com>
2022-10-16 02:37:35 +0500

    Fixed advice app

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import React, { useState, useEffect } from "react"
+import axios from "axios"
+import "./App.css"
+
+const App = () => {
+  const [advice, setAdvice] = useState("")
+
+  const fetchAdvice = () => {
+    axios
+      .get("https://api.adviceslip.com/advice")
+      .then((response) => {
+        const { advice } = response.data.slip
+
+        setAdvice({ advice })
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+  }
+
+  useEffect(() => {
+    fetchAdvice()
+  }, [])
+
+  return (
+    <div className="app">
+      <div className="card">
+        <h1 className="heading">{advice.advice}</h1>
+        <button className="button" onClick={fetchAdvice}>
+          <span>GIVE ME ADVICE!</span>
+        </button>
+      </div>
+    </div>
+  )
+}
+
+export default App
